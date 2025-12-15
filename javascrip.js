6fe8316d1ab211f56adddcc891968a6520dc2614
albertoabel110-gif <albertoabel110@gmail.com>
2025-12-14 21:06:20 -0500

    Update javascrip.js

--- javascrip.js
+++ javascrip.js
@@ -1,19 +1,23 @@
 const tree = document.getElementById("tree");
-const colors = ["red", "gold", "white", "pink"];
+const colors = ["red", "gold", "white"];
 
-// Luces animadas
-for (let i = 0; i < 22; i++) {
+// Luces mejor distribuidas
+for (let i = 0; i < 26; i++) {
     const light = document.createElement("div");
     light.classList.add("light");
-    light.style.top = Math.random() * 190 + "px";
-    light.style.left = Math.random() * 220 - 110 + "px";
+
+    const y = Math.random() * 190;
+    const maxWidth = (190 - y) / 2;
+    light.style.top = y + "px";
+    light.style.left = Math.random() * maxWidth * 2 - maxWidth + "px";
+
     tree.appendChild(light);
 
     setInterval(() => {
         light.style.background =
             colors[Math.floor(Math.random() * colors.length)];
         light.style.color = light.style.background;
-    }, 700);
+    }, 800);
 }
 
 // Nieve
